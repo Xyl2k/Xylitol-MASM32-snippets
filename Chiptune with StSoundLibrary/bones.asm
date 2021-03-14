3a04ebc83af2c59dc7c3933b06be6c23db5b0c50
Steven K <Xyl2k@users.noreply.github.com>
2021-03-14 13:49:58 +0100

    Added ST-Sound Lib code example (YM file player)
    Lib (dynamic + static) with example code

---
+++ Chiptune with StSoundLibrary/bones.asm
@@ -0,0 +1,88 @@
+comment ~---------------------------------------------------------------------
+	Example code to use the YM files player library by Leonard (Arnaud Carre)
+	(http://leonard.oxg.free.fr)
+	
+	by UFO-Pu55y[SnD] 2012, embeded into rc by Xyl2k
+-----------------------------------------------------------------------------~
+
+.386
+.model	flat, stdcall
+option	casemap :none
+
+include \masm32\include\windows.inc
+include \masm32\include\kernel32.inc
+include \masm32\include\user32.inc
+include	\masm32\include\comctl32.inc	;windows common controls
+	
+includelib \masm32\lib\kernel32.lib
+includelib \masm32\lib\user32.lib
+includelib	\masm32\lib\comctl32.lib	;windows common controls
+
+include StSoundLibrary\StSoundLibrary.inc
+
+DlgProc		PROTO	:DWORD,:DWORD,:DWORD,:DWORD
+
+.const
+IDD_MAIN	equ	1000
+IDB_EXIT	equ	1001
+
+.data?
+hInstance	dd ?
+hRes		dd ?
+lenRes		dd ?
+pMusic		dd ?
+	
+.data
+szFileName	db "YMs\\Union Tcb 2.ym", 0
+ID_MSX		EQU 1001
+	
+.code
+start:
+	invoke	GetModuleHandle, NULL
+	mov	hInstance, eax
+	invoke	InitCommonControls
+	invoke	DialogBoxParam, hInstance, IDD_MAIN, 0, offset DlgProc, 0
+	invoke	ExitProcess, eax
+
+DlgProc proc hWin:DWORD,uMsg:DWORD,wParam:DWORD,lParam:DWORD
+	mov	eax,uMsg
+	
+	.if	eax == WM_INITDIALOG
+		invoke	LoadIcon,hInstance,200
+		invoke	SendMessage, hWin, WM_SETICON, 1, eax
+		invoke ymMusicCreate
+		mov pMusic, eax
+		;invoke ymMusicLoad, pMusic, addr szFileName ; Load from file
+		invoke FindResource, hInstance, ID_MSX, RT_RCDATA ; Load from resources
+            .if eax
+				mov hRes, eax
+				invoke SizeofResource, hInstance, eax
+				mov lenRes, eax
+				invoke LoadResource, hInstance, hRes
+				.if eax
+					invoke LockResource, eax
+					.if eax
+						invoke ymMusicLoadMemory, pMusic, eax, lenRes
+					.endif
+				.endif
+			.endif
+					invoke ymMusicSetLoopMode, pMusic, TRUE
+			invoke ymMusicSoundServerStart, pMusic
+		invoke ymMusicPlay, pMusic
+		
+	.elseif eax == WM_COMMAND
+		mov	eax,wParam
+		.if	eax == IDB_EXIT
+				invoke ymMusicSoundServerStop, pMusic
+		invoke ymMusicDestroy, pMusic
+			invoke	SendMessage, hWin, WM_CLOSE, 0, 0
+		.endif
+	.elseif	eax == WM_CLOSE
+		invoke	EndDialog, hWin, 0
+	.endif
+
+	xor	eax,eax
+	ret
+DlgProc endp
+
+end start
