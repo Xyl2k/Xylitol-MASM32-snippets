e97518fd1f314f08b2c37d26489f4bf5edcf83eb
Steven K <Xyl2k@users.noreply.github.com>
2021-05-01 11:22:20 +0200

    Update sysp.asm
    Update sysp.asm

--- GetSystemPowerStatus/sysp.asm
+++ GetSystemPowerStatus/sysp.asm
@@ -1,6 +1,6 @@
-.586
-.model	flat, stdcall
-option	casemap :none
+.386
+.model flat, stdcall  ;32 bit memory model
+option casemap :none  ;case sensitive
 
 include		\masm32\include\windows.inc
 include		\masm32\include\user32.inc
@@ -9,29 +9,42 @@ includelib	\masm32\lib\user32.lib
 includelib	\masm32\lib\kernel32.lib
 include 	\masm32\macros\macros.asm
 
-		
+.data
+szACLine db "GetSystemPowerStatus: ACLineStatus",0	
+szBatteryFlag db "GetSystemPowerStatus: BatteryFlag",0	
+szBatteryLifePercent db "GetSystemPowerStatus: BatteryLifePercent",0
+
 .data?
 sysp		SYSTEM_POWER_STATUS <?>
+
+; typedef struct _SYSTEM_POWER_STATUS {
+;    BYTE  ACLineStatus;
+;    BYTE  BatteryFlag;
+;    BYTE  BatteryLifePercent;
+;    BYTE  SystemStatusFlag;
+;    DWORD BatteryLifeTime;
+;    DWORD BatteryFullLifeTime;
+; } SYSTEM_POWER_STATUS, *LPSYSTEM_POWER_STATUS;
 
 .code
 start:
 invoke GetSystemPowerStatus,addr sysp
 	.if sysp.ACLineStatus == 1
-			invoke MessageBox,NULL,chr$("AC power status: Online"),NULL,MB_ICONINFORMATION
+			invoke MessageBox,NULL,chr$("AC power status: Online"),addr szACLine,MB_ICONINFORMATION
 		.else
 	.if sysp.ACLineStatus == 0
-			invoke MessageBox,NULL,chr$("AC power status: Offline"),NULL,MB_ICONINFORMATION
+			invoke MessageBox,NULL,chr$("AC power status: Offline"),addr szACLine,MB_ICONINFORMATION
 		.endif
 .endif
 	.if sysp.BatteryFlag == 128
-		invoke MessageBox,NULL,chr$("No system battery"),NULL,MB_ICONINFORMATION
+		invoke MessageBox,NULL,chr$("No system battery"),addr szBatteryFlag,MB_ICONINFORMATION
 	ret
 	.else
 		.if	sysp.BatteryLifePercent == 100
-			invoke MessageBox,NULL,chr$("Battery is full"),NULL,MB_ICONINFORMATION
+			invoke MessageBox,NULL,chr$("Battery is full"),addr szBatteryLifePercent,MB_ICONINFORMATION
 			ret
 		.endif
-			invoke MessageBox,NULL,chr$("Battery is not full"),NULL,MB_ICONINFORMATION
+			invoke MessageBox,NULL,chr$("Battery is not full"),addr szBatteryLifePercent,MB_ICONINFORMATION
 			ret
 	.endif
 end start
